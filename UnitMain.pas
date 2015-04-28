c91684714e7e8a368f362deb43840070b7d59bbd
Rene Melo <renemmelo@gmail.com>
2015-04-28 17:02:36 -0300

    Adicionado projeto e tela principal.

---
+++ UnitMain.pas
@@ -0,0 +1,31 @@
+unit UnitMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    ToolBar1: TToolBar;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.iPhone4in.fmx IOS}
+{$R *.iPhone.fmx IOS}
+
+end.
